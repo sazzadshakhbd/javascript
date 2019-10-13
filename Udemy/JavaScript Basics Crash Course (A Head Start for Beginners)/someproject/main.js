41fe9f0f3a515fcfc9ed748f5f0dce760860910a
sazzadshakhbd <sazzadshakhbd@gmail.com>
2019-10-13 15:11:18 +0600

    complete ttutoral

---
+++ Udemy/JavaScript Basics Crash Course (A Head Start for Beginners)/someproject/main.js
@@ -0,0 +1,9 @@
+// How to call variable!
+let myName = "Sazzad";
+alert(myName);
+
+let myAge = 18;
+let p1 = 3.41;
+document.write('Your age is ' + myAge);
+
+
---
+++ Udemy/JavaScript for Beginners - Build Real World Apps (Arabic)/js/variable.js
@@ -0,0 +1,22 @@
+//alert("hello World");
+console.log('hello...')
+console.log(true)
+console.log(25)
+var name = 'sazzad';
+console.log(name);
+console.log([1,2,3,4]);
+console.log({a:1, b:2, c:3});
+console.table({a:1, b:2, c:3});
+
+//console.warn('An errore has occured');
+// console.clear();
+
+
+console.time('t');
+
+console.log([1,2,3,4,5]);
+console.log([1,2,3,4,5]);
+console.log([1,2,3,4,5]);
+console.log([1,2,3,4,5]);
+
+console.timeEnd('t');
