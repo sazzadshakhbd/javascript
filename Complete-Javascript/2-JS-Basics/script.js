e5a48a8f071efeb878eb9f71c5ac801989aff04d
sazzadshakhbd <sazzadshakhbd@gmail.com>
2020-02-09 09:33:49 +0600

    learn string,number,boolean and undefined with camlecase and reserved keyword

--- Complete-Javascript/2-JS-Basics/script.js
+++ Complete-Javascript/2-JS-Basics/script.js
@@ -1,1 +1,19 @@
+// Lecture 1
 console.log('Hello World!');
+
+// Lecture 2 (Variable and Data-types)
+
+var firstName = 'Sazzad'; /* Camelcase */
+console.log(firstName);
+
+var lastname = 'Shakh'; /* String */
+var age = 35; /* Number */
+
+var fullAge = true; /* Boolean */
+console.log(fullAge);
+
+var job; /* Undefined */
+console.log(job);
+
+/* var function = 78;  (Reserved Keyword)
+console.log(function); */
