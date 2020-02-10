c2817b4018443503686b951a5896b096c6d71572
sazzadshakhbd <sazzadshakhbd@gmail.com>
2020-02-10 08:07:43 +0600

    type coretion and varibale mutation

--- Complete-Javascript/2-JS-Basics/script.js
+++ Complete-Javascript/2-JS-Basics/script.js
@@ -1,12 +1,12 @@
 // Lecture 1
-console.log('Hello World!');
+console.log("Hello World!");
 
 // Lecture 2 (Variable and Data-types)
 
-var firstName = 'Sazzad'; /* Camelcase */
+var firstName = "Sazzad"; /* Camelcase */
 console.log(firstName);
 
-var lastname = 'Shakh'; /* String */
+var lastname = "Shakh"; /* String */
 var age = 35; /* Number */
 
 var fullAge = true; /* Boolean */
@@ -17,3 +17,48 @@ console.log(job);
 
 /* var function = 78;  (Reserved Keyword)
 console.log(function); */
+
+// Lecture 3
+console.log(firstName + " " + age); // Type coercion
+
+var designation, isMarried;
+(designation = "Teacher"), (isMarried = true);
+console.log(
+  firstName +
+    " is a " +
+    age +
+    " years old. He is a " +
+    designation +
+    ". Is he Maried? " +
+    isMarried
+);
+
+// this declearation called a variable mutation
+age = "Thirty Five";
+designation = "Front End Developer";
+isMarried = false;
+
+// alert(
+//   firstName +
+//     " is a " +
+//     age +
+//     " years old. He is a " +
+//     designation +
+//     ". Is he Maried? " +
+//     isMarried
+// );
+
+// var lastname = prompt("What is your last name?");
+// console.log(firstName + " " + lastname);
+
+/* Basics Operator */
+var year, sazzadBirthYear, marjiaBirthyear;
+
+now = 2020;
+sazzadBirthYear = now - 37;
+marjiaBirthyear = now - 23;
+
+console.group(now + 2);
+console.group(now * 2);
+console.group(now / 2);
+console.group(now - 2);
