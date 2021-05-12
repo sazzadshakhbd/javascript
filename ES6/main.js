73aa7d285d1f46f00dd87ad7e0ed42958e9a7a28
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-05-12 23:28:26 +0600

    create dynnamic function

--- ES6/main.js
+++ ES6/main.js
@@ -1,10 +1,5 @@
 "use strict";
-function result(x, y, ...marks) {
-  let sum = 0;
-  for (let i of marks) {
-    sum = sum + i;
-  }
-
-  console.log(sum);
-}
-result(100, 200, 10, 20, 30, 40, 50, 60);
+let mystatus = function (x) {
+  return x;
+};
+console.log(mystatus("sazzad"));
