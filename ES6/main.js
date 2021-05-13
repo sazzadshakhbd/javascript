856bd623eb4e6394b44b55ed0e478d3251436542
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-05-13 10:51:30 +0600

    decleare object with nesting object

--- ES6/main.js
+++ ES6/main.js
@@ -1,6 +1,29 @@
 "use strict";
 
-let myFamily = ["MOther", "Marjia", "Sazzad", "Adib", "Adiba"];
-for (let myFamilyMember of myFamily) {
-  console.log(myFamilyMember);
-}
+let tree = {
+  mango: "One Peace",
+  Lichu: "Two Peace",
+  Alovera: "Three Peace",
+  Pudina: "Nine Peace",
+};
+console.log(tree["Pudina"]);
+
+let schoolName = {
+  class: {
+    room: 20,
+    field: 1,
+  },
+  bench: {
+    longbench: 10,
+    benchColor: "Grey",
+  },
+  student: {
+    boys: 200,
+    girls: {
+      baby: 100,
+      big: 100,
+    },
+  },
+};
+
+console.log(schoolName["student"]["girls"]["big"]);
