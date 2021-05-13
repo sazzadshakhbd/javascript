fe7daa6bde88793631cf5d848f0185b8c694c2be
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-05-13 09:57:47 +0600

    create for of loop

--- ES6/main.js
+++ ES6/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let i;
-for (i = 0; i < 10; i = i + 4) {
-  console.log(i);
+let myFamily = ["MOther", "Marjia", "Sazzad", "Adib", "Adiba"];
+for (let myFamilyMember of myFamily) {
+  console.log(myFamilyMember);
 }
