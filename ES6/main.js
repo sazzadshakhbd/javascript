fb322dd14f804a10d6be9d5b28c6ab2237536f11
sazzadshakhbd <sazzadshakhbd@gmail.com>
2021-05-13 11:37:48 +0600

    create simple function

--- ES6/main.js
+++ ES6/main.js
@@ -1,15 +1,9 @@
 "use strict";
 
-let tree = {
-  mango: "One Peace",
-  Lichu: "Two Peace",
-  Alovera: "Three Peace",
-  Pudina: "Nine Peace",
-};
-if (tree["mango"] == "two peach") {
-  console.log("two peace mango");
-} else if (tree["mango"] == "three peace") {
-  console.log("three peace mango");
-} else {
-  console.log("Mango is not found");
+function myCalculation() {
+  var x = 10;
+  var y = 20;
+  var z = x + y;
+  console.log(z);
 }
+myCalculation();
